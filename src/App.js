3c873db464a2babae8ea0b934b56566240b46621
Myron <dubtwee@gmail.com>
2020-07-14 10:47:22 -0500

    converting to class

--- src/App.js
+++ src/App.js
@@ -2,28 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div></div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload everything.
-        </p>
-        <p>Did I reload.</p>
-        <p>SAVED ALL THE THINGS!!!!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        My app is gone
+      </div>
+    );
+  }
 }
 
 export default App;

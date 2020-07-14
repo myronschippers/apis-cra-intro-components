976f65361d06aec4d1d492564272c4fccf315293
Myron <dubtwee@gmail.com>
2020-07-14 10:35:25 -0500

    reloading the application

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div></div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload everything.
         </p>
+        <p>Did I reload.</p>
+        <p>SAVED ALL THE THINGS!!!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"

fa9cd7b96ecaff619877d84b3653f128877e5f7e
Myron <dubtwee@gmail.com>
2020-07-14 11:11:55 -0500

    creating some components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Body from './components/Body/Body';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        My app is gone
+        <Header />
+        <Body />
       </div>
     );
   }
---
+++ src/components/Body/Body.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Body extends Component {
+  render() {
+    return (
+      <p>My app is gone</p>
+    );
+  }
+}
+
+export default Body;
